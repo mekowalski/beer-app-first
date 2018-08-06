4a54a9ccce39707e07e0da23f4c88f89e8a2aaef
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 17:14:14 -0600

    generate company model/db/migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_225644) do
+ActiveRecord::Schema.define(version: 2018_08_06_231213) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.float "abv"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
