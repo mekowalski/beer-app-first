de4f99ae44dfb48f044c0d79ce6ae5e49247cc57
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 17:17:18 -0600

    generate beer style model/db/migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_231213) do
+ActiveRecord::Schema.define(version: 2018_08_06_231555) do
+
+  create_table "beer_styles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
