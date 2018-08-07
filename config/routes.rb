9a5b8eb9bd81f07163588413bd0d47e7b0706bdc
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-07 12:50:39 -0600

    generate beer controller!!!!! notes for resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # create resources for Beer, Company and BeerStyle?
+  # to meet CRUD of the final project
+  # i actually don't know how to incorporate the Update and Delete/Destroy part of the models
+
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
