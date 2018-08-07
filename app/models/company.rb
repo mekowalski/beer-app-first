5670584749eeda6f430020e2d380a0d0da74489c
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-07 10:29:35 -0600

    create relationship for company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ApplicationRecord
+  has_many :beers
+  has_many :beer_styles, through: :beer
 end
+
+# Model: Company
+#   -name
+#   -company has many beers
+#   -company has many styles through beer(?)
