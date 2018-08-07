defbd4424abd23bf8975df6c76e0bb5d870b1387
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-07 10:22:45 -0600

    create relationship for beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,9 @@
 class Beer < ApplicationRecord
+  belongs_to :company
+  belongs_to :beer_style
 end
+
+# Model: Beer
+#   -name, description, ABV, location
+#   -beer belongs to company
+#   -beer belongs to type
